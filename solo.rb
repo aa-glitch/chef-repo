ffceb6a0d406793e8f62e849d732a9114fa72306
Anshul Anand <ansh.anand@gmail.com>
2022-02-01 22:36:06 -0500

    commit solo

--- solo.rb
+++ solo.rb
@@ -1,5 +1,5 @@
 current_dir       = File.expand_path(File.dirname(__FILE__)) 
-file_cache_path   "#{current_dir}" 
-cookbook_path     "#{current_dir}/cookbooks" 
-role_path         "#{current_dir}/roles" 
-data_bag_path     "#{current_dir}/data_bags" 
+file_cache_path   "C:/dev/aws/chef-repo" 
+cookbook_path     "C:/dev/aws/chef-repo/cookbooks" 
+role_path         "C:/dev/aws/chef-repo/roles" 
+data_bag_path     "C:/dev/aws/chef-repo/data_bags" 
